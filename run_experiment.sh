065d1e831e96ce2732ad0fad1a64968204e631f1
gdmst <beatbenz1501@gmail.com>
2025-08-30 02:48:57 -0500

    retrieve replies

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,19 +4,18 @@
 
 # retrieve data since 2025 01 01 until 2025 05 31
 
-start_month=1
-
 # Infinite loop to run the Python script every 2 seconds
+iterations=400
+# while IFS= read -r line
+# do
+#redo 14, 21
 
-while IFS= read -r line
+for ((i=228; i<=iterations; i++))
 do
-    iterations=$line
-    for ((i=13; i<=iterations; i++))
-    do
+
+    python3 query.py --start_day 1 --start_month 1 --position $i
     
-        python3 query.py --start_day $i --start_month $start_month
-        
-        sleep 30m
-    done
-    start_month=$((start_month + 1))
-done < input/month_day_2025.txt
+    # sleep 30m
+done
+    # start_month=$((start_month + 1))
+# done < input/month_day_2025.txt
